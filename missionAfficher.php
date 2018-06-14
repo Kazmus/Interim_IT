f75b3da30957fb5a71c1bd133ec7ad2c875f9ed3
Kazmus <maurizio.girlando@gmail.com>
2018-06-14 02:15:59 +0200

    V0.87C

--- missionAfficher.php
+++ missionAfficher.php
@@ -24,15 +24,15 @@ try {
 				$tablePostuler = $bdd->query("SELECT * FROM postuler WHERE ID_Info= '" . $_SESSION['id'] . "' AND ID_Mission='" . $data['ID_Mission'] . "' ");
 				$tableEngager = $bdd->query("SELECT ID_Info,ID_Mission FROM engager WHERE ID_Info= '" . $_SESSION['id'] . "' AND ID_Mission='" . $data['ID_Mission'] . "' ");
 				affichageMission($data['ID_Mission'], $data['Type_Mission'], $data['Titre'], $data['Lieu'], $data['Date_Debut'], $data['Date_Fin'], $data['Effectif_Requis'], $data['Description'], $data['Remuneration'], $data['Reservation_Max'], $data['Nom']);
-				if ($tablePostuler->rowCount() != 1) {
-				?><form method="post" action="postuler.php">
+				if ($tableEngager->rowCount() == 1) {
+					?><strong>|--Vous etes deja Engager--|</strong><?php
+				} else if ($tablePostuler->rowCount() == 1) {
+				  	?><strong>|--DEJA Postuler--|</strong><?php
+				} else {
+				  ?><form method="post" action="postuler.php">
 					<input type="hidden" name="hiddenIdMission" value="<?php echo $data['ID_Mission'];?>"/>
 					<input class="button" name='submit' type="submit" value="Postuler" />
 				</form><?php
-				} else if ($tableEngager->rowCount() == 1) {
-				  ?><strong>|--Vous etes deja Engager--|</strong><?php
-				} else {
-				  ?><strong>|--DEJA Postuler--|</strong><?php
 				}
 				
 			}
